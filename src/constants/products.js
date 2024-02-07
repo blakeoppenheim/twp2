73773a076182313ba1b82cc1a16fb953851ba28d
wassim fraj <wassim.fraj@horizon-tech.tn>
2024-02-07 01:04:51 +0100

    add change products logic

---
+++ src/constants/products.js
@@ -0,0 +1,38 @@
+import glassBottleIcon from "./../assets/icons/glassBottle.svg";
+import plasticBottleIcon from "./../assets/icons/plasticBottle.svg";
+import aluminumBottleIcon from "./../assets/icons/aluminumBottle.svg";
+export const glassBottle = {
+  id: 1,
+  specialWord: "",
+  type: "glass",
+  icon: glassBottleIcon,
+  iconDescription:
+    "Choose our glass bottle for a taste of purity. It's naturally sustainable, chemical-free, and keeps your water refreshingly crisp.",
+  title: "PURE",
+  description:
+    "Opt for the timeless clarity of glass, providing a toxin-free experience while supporting sustainability with every sip.",
+};
+
+export const plasticBottle = {
+  id: 2,
+  type: "plastic",
+  specialWord: "",
+  icon: plasticBottleIcon,
+  iconDescription:
+    "Our lightweight plastic bottle is made from recycled materials, combining convenience with environmental responsibility.",
+  title: "LIGHTWEIGHT",
+  description:
+    "Crafted from lightweight plastics, this bottle offers a guilt-free way to stay hydrated, blending eco-friendliness with everyday utility.",
+};
+
+export const aluminumBottle = {
+  id: 2,
+  type: "aluminum",
+  specialWord: "eco-friendly on-the-go hydration.",
+  icon: aluminumBottleIcon,
+  iconDescription:
+    "High-quality aluminum combines strength, corrosion resistance, and light weight, making it ideal for maintaining temperature",
+  title: "ECO-SMART",
+  description:
+    "Lightweight and durable, our aluminum bottle is ideal for maintaining your drink's temperature, perfect for",
+};
--- src/features/Products/index.jsx
+++ src/features/Products/index.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  glassBottle,
+  plasticBottle,
+  aluminumBottle,
+} from "./../../constants/products";
 import Button from "./../../components/ui/Button";
 import Icon from "./../../assets/icons/aliminumBottle.svg";
 import waterIcon from "./../../assets/icons/water.svg";
@@ -11,40 +16,67 @@ import plasticIconBlue from "./../../assets/icons/bottleBlue.svg";
 import glassIconBlue from "./../../assets/icons/glass.svg";
 import glassIconWhite from "./../../assets/icons/glassWhite.svg";
 function Products() {
+  const [selectedProduct, setSelectedProduct] = useState(aluminumBottle);
   return (
     <section className="products-container">
       <article className="products-container__header">
-        <div className="product-image__container">
-          <img src={Icon} className="bottle-image" alt="product-image" />
-          <p className="image-description">
-            High-quality aluminum combines strength, corrosion resistance, and
-            light weight, making it ideal for maintaining temperature
-          </p>
+        <div data-aos="fade-right" className="product-image__container">
+          <img
+            src={selectedProduct.icon}
+            className="bottle-image"
+            alt="product-image"
+          />
+          <p className="image-description">{selectedProduct.iconDescription}</p>
         </div>
-        <div className="product-details__container">
-          <h5 className="product-material">ALUMINUM</h5>
+        <div data-aos="fade-left" className="product-details__container">
+          <h5 className="product-material">{selectedProduct.type}</h5>
           <h1 className="product-title">
-            ECO-SMART <br /> <span className="title">ALUMINUM</span>
+            {selectedProduct.title} <br />{" "}
+            <span className="title">{selectedProduct.type}</span>
             <span className="point">.</span>
           </h1>
           <p className="product-detail">
-            Lightweight and durable, our aluminum bottle is ideal for
-            maintaining your drink's temperature, perfect for <br />
-            <span className="special-word">eco-friendly</span>
-            on-the-go hydration.
+            {selectedProduct.description}
+            <span>
+              {selectedProduct.specialWord && selectedProduct.specialWord}
+            </span>
           </p>
           <div className="buttons-action__container">
             <Button
-              className="active"
+              onClick={() => setSelectedProduct(aluminumBottle)}
+              className={selectedProduct.type === "aluminum" ? "active" : ""}
               icon={aluminumIconBlue}
               text="Aluminum"
+              icon={
+                selectedProduct.type === "aluminum"
+                  ? aluminumIconBlue
+                  : aluminumIconWhite
+              }
             />
-            <Button icon={plasticIconWhite} text="Plastic" />
-            <Button icon={glassIconWhite} text="Glass" />
+            <Button
+              onClick={() => setSelectedProduct(plasticBottle)}
+              icon={
+                selectedProduct.type === "plastic"
+                  ? plasticIconBlue
+                  : plasticIconWhite
+              }
+              text="Plastic"
+              className={selectedProduct.type === "plastic" ? "active" : ""}
+            />
+            <Button
+              onClick={() => setSelectedProduct(glassBottle)}
+              icon={
+                selectedProduct.type === "glass"
+                  ? glassIconBlue
+                  : glassIconWhite
+              }
+              text="Glass"
+              className={selectedProduct.type === "glass" ? "active" : ""}
+            />
           </div>
         </div>
       </article>
-      <article className="products-container__footer">
+      <article  className="products-container__footer">
         <ul className="list">
           <li>
             <img
