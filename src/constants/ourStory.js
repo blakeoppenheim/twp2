2e93436dd9c91787a325b4b92a608d274b922f5a
Blake Oppenheim <129343880+blakeoppenheim@users.noreply.github.com>
2024-02-26 16:50:16 +0100

    Update ourStory.js

--- src/constants/ourStory.js
+++ src/constants/ourStory.js
@@ -3,7 +3,7 @@ export const OUR_STORY_DATA = {
   title: "OUR STORY",
   description: {
     firstSection:
-      "In 1996, our founder sold a named brand door-to-door in NYC. In 1997, Graces Market embraced the idea of private-labeled water, becoming the first. Subsequently, New York Health and Racquetball Club, Macy's, Dean & Deluca, The Amish Market, Burke & Burke, Devon & Blakley, Bloomingdale's, Macys, NikeTown, and others joined. Inspired by the belief 'Why sell Evian when you can sell yourself', TheWaterPeople, with 27 years of trust, remains the top choice for businesses seeking quality, experience, and reliable service.",
+      "In 1996, our founder sold a named brand door-to-door in NYC. In 1997, Graces Market embraced the idea of private-labeled water, becoming the first. Subsequently, New York Health and Racquetball Club, Macy's, Dean & Deluca, The Amish Market, Burke & Burke, Devon & Blakley, Bloomingdale's, Macys, NikeTown, and others joined. Inspired by the belief, \"Why sell Evian when you can sell yourself?\" TheWaterPeople, with 27 years of trust, remains the top choice for businesses seeking quality, experience, and reliable service.",
   },
   icon: ourStoryIcon,
   hasBackgroundIcon: false,
